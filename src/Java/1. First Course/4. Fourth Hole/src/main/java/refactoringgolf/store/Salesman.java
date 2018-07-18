48b2a93747a710090484fe5c059656dc0253b5e4
Krzysztof Jelski <kjelski@gmail.com>
2018-07-18 07:21:22 +0200

    Mavenize.

---
+++ src/Java/1. First Course/4. Fourth Hole/src/main/java/refactoringgolf/store/Salesman.java
@@ -0,0 +1,32 @@
+package refactoringgolf.store;
+
+public class Salesman extends Employee {
+
+    private int commissionPercentage;
+    private float monthQuota;
+
+    public Salesman(String firstName, String lastName, float fixedSalary, int commissionPercentage) {
+        super(firstName, lastName, fixedSalary);
+        this.commissionPercentage = commissionPercentage;
+    }
+
+    public void setCommissionPercentage(int commissionPercentage) {
+        this.commissionPercentage = commissionPercentage;
+    }
+
+    public int getCommissionPercentage() {
+        return commissionPercentage;
+    }
+
+    public float getMonthQuota() {
+        return monthQuota;
+    }
+
+    public void updateMonthQuota(float addQuota) {
+        monthQuota = monthQuota + addQuota;
+    }
+
+    protected float benefits() {
+        return monthQuota * commissionPercentage / 100;
+    }
+}
